aeed1d1b718a907b8491ffbd58b811f1271ead65
runei <leo.invernizzi@hotmail.com>
2017-09-29 08:58:51 -0300

    first commit

---
+++ botoes.ino
@@ -0,0 +1,151 @@
+#include <Wire.h>
+
+int clock = 8;
+int reset = 9;
+
+void shift_bit(){
+  digitalWrite(clock, HIGH);
+  digitalWrite(clock, LOW);
+}
+
+void reset_shift(){
+  digitalWrite(reset, HIGH);
+  shift_bit();
+  digitalWrite(reset, LOW);
+}
+
+void reset_matrix(){
+  int i;
+  for(i=0;i<12;i++){
+    shift_bit();
+  }
+  //registo A integrado da esquerda
+  Wire.beginTransmission(0x24);
+  Wire.write(0x12); // address port A
+  Wire.write(0xFF);  // desliga saidas
+  Wire.endTransmission();
+  
+  //registo B integrado da esquerda
+  Wire.beginTransmission(0x24);
+  Wire.write(0x13); // address port B
+  Wire.write(0xFF);  // desliga saidas
+  Wire.endTransmission();
+
+  //registo A integrado do meio
+  Wire.beginTransmission(0x26);
+  Wire.write(0x12); // address port A
+  Wire.write(0xFF);  // desliga saidas
+  Wire.endTransmission();
+
+  //registo B integrado do meio
+  Wire.beginTransmission(0x26);
+  Wire.write(0x13); // address port B
+  Wire.write(0xFF);  // desliga saidas
+  Wire.endTransmission();
+
+  //registo A integrado do direita
+  Wire.beginTransmission(0x27);
+  Wire.write(0x12); // address port A
+  Wire.write(0xFF);  // desliga saidas
+  Wire.endTransmission();
+
+  //registo B integrado da direita
+  Wire.beginTransmission(0x27);
+  Wire.write(0x13); // address port B
+  Wire.write(0xFF);  // desliga saidas
+  Wire.endTransmission();
+}
+
+void setup_cubo(){
+  Wire.begin();
+
+  //integrado da esquerda
+  Wire.beginTransmission(0x24);
+  Wire.write(0x00); // IODIRA register
+  Wire.write(0x03); // registos 0 e 1 como saídas e resto como entradas
+  Wire.endTransmission();
+  
+  Wire.beginTransmission(0x24);
+  Wire.write(0x01); // IODIRB register
+  Wire.write(0xC0); // registos 7 e 6 como saídas e resto como entradas
+  Wire.endTransmission();
+
+
+  //integrado da meio
+  Wire.beginTransmission(0x26);
+  Wire.write(0x00); // IODIRA register
+  Wire.write(0x03); // registos 0 e 1 como saídas e resto como entradas
+  Wire.endTransmission();
+
+  Wire.beginTransmission(0x26);
+  Wire.write(0x01); // IODIRB register
+  Wire.write(0xC0); // registos 7 e 6 como saídas e resto como entradas
+  Wire.endTransmission();
+
+  //integrado da direita
+  Wire.beginTransmission(0x27);
+  Wire.write(0x00); // IODIRA register
+  Wire.write(0x03); // registos 0 e 1 como saídas e resto como entradas
+  Wire.endTransmission();
+
+  //resistencias de pull_up
+  Wire.beginTransmission(0x27);
+  Wire.write(0x0C); // address port A
+  Wire.write(0x03);  // value to send
+  Wire.endTransmission();
+  
+  Wire.beginTransmission(0x27);
+  Wire.write(0x01); // IODIRB register
+  Wire.write(0xC0); // registos 7 e 6 como saídas e resto como entradas
+  Wire.endTransmission();
+
+  //resistencias de pull_up
+  Wire.beginTransmission(0x27);
+  Wire.write(0x0D); // address port A
+  Wire.write(0xC0);  // value to send
+
+  //Configurar pinos do shift register
+  pinMode(clock, OUTPUT); //clock
+  pinMode(reset, OUTPUT); //reset
+
+  reset_matrix();
+}
+
+void setup() {
+  setup_cubo();
+}
+
+void loop() {
+  byte inA,inB;
+  Serial.begin(9600);
+  //Serial.print("I received: ");
+  //Wire.beginTransmission(0x27);
+  //Wire.write(0x13);
+
+  Wire.requestFrom(0x27, 2);
+
+  while(Wire.available())    // slave may send less than requested
+  {
+    inA = Wire.read();
+    inB = Wire.read();
+    inA = inA & 0x03;
+    inB = inB & 0xC0;
+    //botao cima 1
+    if(inA == 1){
+        Serial.print("Carregou no botao de cima\n");
+    }
+    //botao baixo 2
+    if(inA == 2){
+        Serial.print("Carregou no botao de baixo\n");
+    }
+    
+  }
+  delay(50);
+  //in = ini [0];
+  //delay(100);
+
+  // say what you got:
+  //Serial.print("\nI received: ");
+  //Serial.println(in, DEC);
+
+}
---
+++ preentivo.ino
@@ -0,0 +1,122 @@
+//timer interrupts
+//timer setup for timer1.
+//#define ledPin 13
+
+typedef struct {
+  /* period in ticks */
+  int period;
+  /* ticks until next activation */
+  int delay;
+  /* function pointer */
+  void (*func)(void);
+  /* activation counter */
+  int exec;
+} Sched_Task_t;
+
+Sched_Task_t Tasks[20];
+int cur_task = 20;
+
+void Sched_Schedule(void){
+  int x;
+  for(x=0; x<20; x++) {
+    if((Tasks[x].func) && (Tasks[x].delay)){
+      Tasks[x].delay--;
+      if(!Tasks[x].delay){
+        /* Schedule Task */
+        Tasks[x].exec++;
+        Tasks[x].delay = Tasks[x].period;
+      }
+    }
+  }
+}
+
+int Sched_Dispatch(void){
+  int prev_task = cur_task;
+  int x;
+  for(x=0; x<20; x++) {
+    if((Tasks[x].func) && (Tasks[x].exec)) {
+      Tasks[x].exec--;
+      cur_task = x;
+      interrupts();
+      Tasks[x].func();
+      noInterrupts();
+      cur_task = prev_task;
+      // Delete task if one-shot
+      if(!Tasks[x].period)
+        Tasks[x].func = 0;
+      return 0;
+    }
+  }
+}
+
+void int_handler(void){
+  noInterrupts();
+  Sched_Schedule();
+  Sched_Dispatch();
+  interrupts();
+};
+
+int Sched_Init(void){
+  int x;
+  for(x=0; x<20; x++){
+    Tasks[x].func = 0;
+  }
+
+  //Configure interrupts
+  noInterrupts(); // disable all interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+ 
+  //OCR1A = 31250; // compare match register 16MHz/256/2Hz
+  OCR1A = 31;    // compare match register 16MHz/256/2kHz
+  TCCR1B |= (1 << WGM12); // CTC mode
+  TCCR1B |= (1 << CS12); // 256 prescaler
+  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+  interrupts(); // enable all interrupts
+
+  int_handler();
+}
+
+int Sched_AddT(void (*f)(void),int d, int p){
+  int x;
+  for(x=0; x<20; x++)
+    if (!Tasks[x].func) {
+      Tasks[x].period = p;
+      Tasks[x].delay = d;
+      Tasks[x].exec = 0;
+      Tasks[x].func = f;
+      return x;
+    }
+  return -1;
+}
+
+ISR(TIMER1_COMPA_vect){//timer1 interrupt
+  int_handler();
+}
+
+void FuncX(void){
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  //digitalWrite(11, HIGH);
+}
+
+void FuncY(void){
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+}
+
+void setup(){
+  
+  pinMode(11, OUTPUT); 
+  pinMode(12, OUTPUT);  
+  Sched_Init();
+  Sched_AddT(FuncX, 1, 2000);
+  Sched_AddT(FuncY, 2, 20);
+  
+}//end setup
+  
+void loop(){
+  //do other things here
+  //Sched_Dispatch();
+}
